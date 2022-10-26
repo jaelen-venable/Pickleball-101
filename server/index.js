4b7018cd0903ccfacd858fc24411e5990115f3c1
Jaelen Venable <jav91960@yahoo.com>
2022-10-26 12:54:13 -0400

    updated

--- server/index.js
+++ server/index.js
@@ -6,3 +6,18 @@ import cors from 'cors'
 const app = express();
 
 app.use(bodyParser.json({limit: "20mb", extended:true}))
+app.use(bodyParser.urlencoded({limit: "20mb", extended:true}))
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://jaeven:2723@cluster0.elmgakk.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { 
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(() => app.listen(PORT, () =>
+console.log(`Connection is established and running on port: ${PORT}`)
+)).catch((err) => console.log(err.message));
+
+
