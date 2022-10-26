3927bbac61d7e1fd5bfbd210c8bbc70ffb28e495
Jaelen Venable <jav91960@yahoo.com>
2022-10-26 19:55:43 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import './App.css';
+import { mergeClasses } from '@material-ui/styles';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container maxWidth="lg">
+        <AppBar className={classes.AppBar}></AppBar>
+      </Container>
+
     </div>
   );
 }
