17d4a3b80e5aa02a03242ed3d6b34c55d15d7e58
Jaelen Venable <jav91960@yahoo.com>
2022-10-26 19:41:14 -0400

    revision

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>PickleBall 101</h1>
-      </header>
+      
     </div>
   );
 }
