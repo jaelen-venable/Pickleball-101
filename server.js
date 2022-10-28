021ed9657c27a9d80f66e74cd5b26e341759b262
Jaelen Venable <jav91960@yahoo.com>
2022-10-27 23:14:54 -0400

    db

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express()
 const path = require('path')
 const {logger} = require('./custom/logger')
 const errorHandle = require('./custom/errorHandle')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
+app.use(cors())
+
 app.use(express.json())
 
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
