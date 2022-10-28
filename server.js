6ba2fc220ce66da732ec591520c03f51c7c81f95
Jaelen Venable <jav91960@yahoo.com>
2022-10-27 22:52:57 -0400

    server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./custom/logger')
+const errorHandle = require('./custom/errorHandle')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
+
+app.use(express.json())
+
+
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+
+app.use(errorHandle)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
